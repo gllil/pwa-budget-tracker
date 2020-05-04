7d478463639d4b850af82f5fbe2ee560a46385d4
Gary Llil <garyjllil@gmail.com>
2020-05-04 12:26:32 -0400

    added process.env.PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
